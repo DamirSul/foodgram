02c9f05c840ceb08872460b94f849ee8399acbc8
DamirSul <_@example.com>
2024-09-15 23:04:49 +0600

    add admin panel

---
+++ backend/food/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+
+from .models import (
+    User,
+    Tag,
+    Recipe,
+    Purchase,
+    Favorite,
+    Subscription,
+    Ingredient,
+    RecipeIngredient
+)
+
+admin.site.register(
+    User,
+    Tag,
+    Recipe,
+    Purchase,
+    Favorite,
+    Subscription,
+    Ingredient,
+    RecipeIngredient
+)

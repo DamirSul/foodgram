c3c68f5a7abc3b92a557acae898379fdc9c88091
DamirSul <_@example.com>
2024-10-13 16:50:35 +0600

    error commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -114,7 +114,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 6,
 }
-SITE_DOMAIN = 'localhost:8000'
+SITE_DOMAIN = 'damirsite.site'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
